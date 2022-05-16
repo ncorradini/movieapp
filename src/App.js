04646b1626ec29d0c49791dceb39a3c032330633
Nicolás Corradini <nicocorradini471@gmail.com>
2022-05-16 11:23:49 -0300

    Created custom theme

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import Footer from './components/Footer';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from './config/costumTheme';
 import Header from './components/Header';
 import Routing from './routes/routes';
+import Footer from './components/Footer';
 import { popularMoviesRequest } from './services/movieServices';
+import { CssBaseline } from '@mui/material';
 
 const App = () => {
   popularMoviesRequest();
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Header />
       <Routing />
       <Footer />
-    </>
+    </ThemeProvider>
   );
 };
 
---
+++ src/config/costumTheme.js
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  palette: {
+    background: {
+      default: '#080808',
+    },
+    primary: {
+      main: '#ff0800',
+      contrastText: '#fff',
+    },
+    secondary: {
+      main: '#fff',
+    },
+  },
+});
