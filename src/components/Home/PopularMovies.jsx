0876a75b2bc74c1111a3379678baf74b19da3d76
Nicolás Corradini <nicocorradini471@gmail.com>
2022-05-19 14:46:33 -0300

    Created popular section and popular slice (redux)

--- src/components/Home/PopularMovies.jsx
+++ src/components/Home/PopularMovies.jsx
@@ -1,5 +1,5 @@
 import { Box, Container, Typography } from '@mui/material';
-import MovieCard from './MovieCard';
+import MovieCard from '../MovieCard';
 
 const PopularMovies = ({ movies }) => {
   return (
--- src/store/slices/popularMovies.js
+++ src/store/slices/popularMovies.js
@@ -5,23 +5,30 @@ export const popularMoviesSlice = createSlice({
   name: 'popularMovies',
   initialState: {
     list: [],
+    loading: false,
   },
   reducers: {
     setPopularMoviesList: (state, action) => {
       state.list = action.payload;
     },
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    },
   },
 });
 
-export const { setPopularMoviesList } = popularMoviesSlice.actions;
+export const { setPopularMoviesList, setLoading } = popularMoviesSlice.actions;
 
 export default popularMoviesSlice.reducer;
 
 export const fetchPopularMovies = () => async (dispatch) => {
+  dispatch(setLoading(true));
   try {
     const response = await popularMoviesRequest();
     dispatch(setPopularMoviesList(response));
   } catch (error) {
     console.log(error);
+  } finally {
+    dispatch(setLoading(false));
   }
 };
