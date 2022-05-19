3f5a6482c0573a35e9ac49b19bb0fb67fb9fad71
Nicolás Corradini <nicocorradini471@gmail.com>
2022-05-19 14:47:24 -0300

    Created store redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import popularMovies from './slices/popularMovies';
+import actionMovies from './slices/actionMovies';
+import horrorMovies from './slices/horrorMovies';
+import dramaMovies from './slices/dramaMovies';
+import animationMovies from './slices/animationMovies';
 import genre from './slices/genre';
 
 export default configureStore({
   reducer: {
     popularMovies,
     genre,
+    actionMovies,
+    horrorMovies,
+    dramaMovies,
+    animationMovies,
   },
 });
