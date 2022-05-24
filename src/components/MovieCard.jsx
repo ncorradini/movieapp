de982d51b7759da822870fec56e9122fde184851
Nicolás Corradini <nicocorradini471@gmail.com>
2022-05-24 11:24:54 -0300

    Added effect hover in movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import { Card, Box } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import SlowMotionVideoIcon from '@mui/icons-material/SlowMotionVideo';
 
 const MovieCard = ({ movie }) => {
   const navigate = useNavigate();
@@ -15,10 +16,34 @@ const MovieCard = ({ movie }) => {
         sx={{
           width: '100%',
           height: '300px',
+          background: 'rgba(0, 0, 0, 0.7)',
           backgroundImage: `url(${process.env.REACT_APP_URL_IMAGE}/${movie?.poster_path})`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
+          position: 'relative',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          transition: '0.5s',
+          '&:hover': { backgroundBlendMode: 'darken', transform: 'scale(1.25)' },
         }}>
+        <Box sx={{
+          width: '100%',
+          height: '100%',
+          position: 'absolute',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          cursor: 'pointer',
+          transition: '0.5s',
+          opacity: '0',
+          '&:hover': { opacity: '0.6' },
+        }}>
+          <SlowMotionVideoIcon sx={{
+            color: '#fff',
+            fontSize: '80px',
+          }} />
+        </Box>
       </Box>
     </Card>
   );
